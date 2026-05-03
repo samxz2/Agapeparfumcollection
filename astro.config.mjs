68135b4291eeabf33afea21130c242fb5728de01
samxz2 <sam14tv14@gmail.com>
2026-05-03 13:06:10 -0400

    Fix: eliminar Tailwind temporalmente para que build pase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue()]
 });
