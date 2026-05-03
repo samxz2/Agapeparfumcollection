c8825e5f9c2458a23804dcb0c4e92ad65ff258c1
samxz2 <sam14tv14@gmail.com>
2026-05-03 12:49:39 -0400

    Fix: Ver mas funciona en produccion con client:only

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
 import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [vue()]
+  integrations: [vue(), tailwind()],
+  output: 'static'
 });
