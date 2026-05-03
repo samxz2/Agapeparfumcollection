213d9adf090b299ebf6fd2759f2b4b0a2742f0ec
samxz2 <sam14tv14@gmail.com>
2026-05-03 12:58:50 -0400

    Fix: usar @astrojs/tailwind en lugar de @tailwindcss/vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [vue(), tailwind()],
-  output: 'static'
+  integrations: [vue(), tailwind()]
 });
